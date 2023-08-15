94055db55b79306bbff4a84f1f79ebcea4d0ac96
Laurence Luo <admin@lzc.app>
2023-08-15 21:47:26 +0800

    Update AssemblyInfo.cs

--- MikuWeather_CS/Properties/AssemblyInfo.cs
+++ MikuWeather_CS/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MikuWeather Windows")]
-[assembly: AssemblyCopyright("Copyright RainySummer ©  2019")]
+[assembly: AssemblyCopyright("Copyright RainySummer ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.9.*")]
-[assembly: AssemblyFileVersion("3.9")]
+[assembly: AssemblyVersion("3.9.6")]
+[assembly: AssemblyFileVersion("3.9.6")]
 [assembly: NeutralResourcesLanguage("zh")]
